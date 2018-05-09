e53134f931663073d7f4779dbddc1a0ea39f47c7
arik-ku <18296112413@163.com>
2018-05-09 17:50:01 +0800

    ##项目构建

---
+++ business-system/business-system.notice/src/main/java/org/business/system/notice/App.java
@@ -0,0 +1,13 @@
+package org.business.system.notice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
